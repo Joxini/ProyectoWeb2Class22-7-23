fbe8cd24d8271d11623f67573922cc404647680f
Joxini <jocksanvargas15@gmail.com>
2023-07-22 11:47:20 -0600

    first commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+const lista = [MatButtonModule, MatCardModule,
+  MatTableModule,MatInputModule,MatFormFieldModule
+
+]
+@NgModule({
+    //los ... es para meter los elementos de la lista
+    exports: [...lista],
+    imports: [...lista],
+  })
+  export class MaterialModule {}
